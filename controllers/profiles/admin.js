5ba99d614fbd8232981740d0cef7b41a7dd52bfc
nohaiz <nohaiznazier@gmail.com>
2024-08-17 01:29:25 +0300

    admin CRUD completed.

--- controllers/profiles/admin.js
+++ controllers/profiles/admin.js
@@ -3,8 +3,27 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
 
+// MODELS
 const User = require("../../models/user");
 const Admin = require("../../models/admin");
+
+// VIEW ALL ADMIN
+
+router.get("/:userId/admins", async (req, res) => {
+  if (req.user.type[2000]) {
+    try {
+      const admins = await Admin.find({});
+
+      res.json(admins);
+    } catch (error) {
+      res.status(404).json({ error: error.message });
+    }
+  } else {
+    res.status(404).json({ error: "Oops, something went wrong" });
+  }
+});
+
+// CREATE ADMIN
 
 router.post("/:userId/admins", async (req, res) => {
   if (req.user.type[2000]) {
@@ -29,6 +48,42 @@ router.post("/:userId/admins", async (req, res) => {
     }
   } else {
     res.status(404).json({ error: "Oops, something went wrong" });
+  }
+});
+
+// VIEW DOCTOR
+
+router.get("/:userId/admins/:id", async (req, res) => {
+  try {
+    const admin = await Admin.findById(req.params.id);
+
+    res.json(admin);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+});
+
+// UPDATE DOCTOR
+
+router.put("/:userId/admins/:id", async (req, res) => {
+  try {
+    const admin = await Admin.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.json({ message: "Admin Updated" }, admin);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+});
+
+// DELETE DOCTOR
+
+router.delete("/:userId/admins/:id", async (req, res) => {
+  try {
+    const admin = await Admin.findByIdAndDelete(req.params.id);
+    res.json({ message: "Admin Deleted" }, admin);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
   }
 });
 
