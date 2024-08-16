089062a182cc45dbd7216dfc84a2db07810b43fb
Qasim <qalhamad33@gmail.com>
2024-08-16 21:03:14 +0300

    profile routes and doctor model

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const verifyToken = require("./middlewares/verify-token");
 
 // DATABASE
 
@@ -17,16 +18,21 @@ const port = process.env.PORT ? process.env.PORT : "3000";
 
 // CONTROLLERS
 const usersRouter = require("./controllers/users.js");
+const profileRouter = require("./controllers/profile.js");
 
 // MIDDLEWARE
 
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(verifyToken);
 
 // ROUTES
 app.use("/users", usersRouter);
+app.use("/users", profileRouter);
 
 app.listen(port, () => {
   console.log("The express app is ready!");
 });
+
+// Testing
